8b1b7ab8ac7a75b49152a6d0d1a1d2a2bb4dcb16
Dipanjan Patra <patraanjan23@gmail.com>
2021-01-08 03:45:06 +0530

    added lazy loading for img elements, updated css

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from "react"
 function Card(props) {
     return (
         <div className="card">
-            <img width="200px" src={props.data.extraAssets.charaGraph.ascension[1]} alt="placeholder"/>
+            <img width="512px" height="724px" loading="lazy" src={props.data.extraAssets.charaGraph.ascension[1]} alt="placeholder"/>
             <h3 className="sname">{props.data.name}</h3>
             <h2 className="sclass">{props.data.className}</h2>
         </div>
